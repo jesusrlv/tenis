fbd6ebf1a7742522b6262ee332631ef7252b2a6c
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-05-16 20:34:49 -0500

    adecuacion rastreo de pedido

--- envio.php
+++ envio.php
@@ -9,7 +9,10 @@
     <title>Tienda en línea · Envíos</title>
     <link rel="icon" type="image/png" href="assets/brand/img/cel.ico">
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     
@@ -103,11 +106,10 @@
                 <table class="table text-center">
                     <thead class="bg-dark text-light">
                         <tr>
-                        <th scope="col">Compañía</th>
-                        <th scope="col">Fecha llegada</th>
-                        <th scope="col">Costo envío</th>
-                        <th scope="col">Código de producto</th>
-                        <th scope="col">Código de rastreo</th>
+                        <th scope="col">Fecha pedido</th>
+                        <th scope="col">Listado de pedido</th>
+                        <th scope="col">Código envío</th>
+                        <th scope="col">Estatus del envío</th>
                         </tr>
                     </thead>
                     <?php
--- query/query_rastreo.php
+++ query/query_rastreo.php
@@ -14,13 +14,43 @@ include('qconn/qc.php');
             while($row_sql_envio = $resultado_sql_busqueda->fetch_assoc()){
                 echo '<tbody>
                 <tr>
-                <th scope="row" class="table-danger">'.$row_sql_envio['compania'].'</th>
-                <td class="table-primary">'.$row_sql_envio['fecha_llegada'].'</td>
-                <td class="table-primary">$ '.$row_sql_envio['costo_envio'].'</td>
-                <td class="table-primary">'.$row_sql_envio['codigo_envio_interno'].'</td>
-                <td class="table-primary">'.$row_sql_envio['codigo_envio_externo'].'</td>
+                <td class="table-primary">'.$row_sql_envio['fecha_registro'].'</td>
+                <td class="table-primary"><a data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-card-list h4"></i></a></td>
+                <td class="table-primary">'.$row_sql_envio['codigo_envio_interno'].'</td>';
+                if($row_sql_envio['codigo_envio_interno']==1){
+                    echo '<td class="table-primary"><i class="bi bi-check-circle-fill text-success"></i> Entregado</td>';
+                }
+                else{
+                    echo '<td class="table-primary"><span class="badge text-bg-warning"><i class="bi bi-exclamation-circle-fill"></i> No entregado</span></td>';
+                }
+                echo '
                 </tr>
             </tbody>';
+
+            echo '<!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Lista de pedido</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">';
+                        $idPedido = $row_sql_envio['codigo_envio_interno'];
+                        $sqlPedido = "SELECT * FROM venta_individual WHERE venta_gral = '$idPedido'";
+                        $resultado_sqlPedido= $conn->query($sqlPedido);
+                        while($row_sqlPedido = $resultado_sqlPedido->fetch_assoc()){
+                            echo '<p>'.$row_sqlPedido['producto'].' | '.$row_sqlPedido['fecha_venta'].'</p>';
+                        }
+
+                    echo '</div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>';
             }
         }
         elseif($resultado_rows == 0){
