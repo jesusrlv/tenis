863743abfb573bf7ea1610d0c83270b20f675ead
jesusrlv <jesusrlvrojo@gmail.com>
2022-06-02 16:06:10 -0500

    tabindex

--- query/query_rastreo.php
+++ query/query_rastreo.php
@@ -28,7 +28,7 @@ include('qconn/qc.php');
             </tbody>';
 
             echo '<!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="exampleModal" tabindex="1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                     <div class="modal-header">
