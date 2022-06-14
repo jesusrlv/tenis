036f8b160b4a38c9b7936a0c509a913d698878fa
jesusrlv <jesusrlvrojo@gmail.com>
2022-06-14 13:27:24 -0500

    logo app

--- be_/index.php
+++ be_/index.php
@@ -42,7 +42,8 @@
     
 <main class="form-signin">
   <form action="login.php" method="POST" id="pwdForm">
-  <i class="bi bi-box-seam" style="font-size:81px"></i><!-- <img class="mb-4" src="../assets/brand/img/cel.png" alt="" width="72" height="72"> -->
+  <!-- <i class="bi bi-box-seam" style="font-size:81px"></i> -->
+  <img class="mb-4" src="../assets/brand/img/logo_store_shoes_sin_fondo.png" alt="" width="72" height="72">
     <h1 class="h3 mb-3 fw-normal"><i class="bi bi-key-fill"></i> Acceso</h1>
 
     <div class="form-floating">
