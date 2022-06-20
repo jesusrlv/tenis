ea347565aadbffd1212fbd432f97119be40d5fd9
jesusrlv <jesusrlvrojo@gmail.com>
2022-06-20 13:41:11 -0500

    acomodo de img

--- catalogo.php
+++ catalogo.php
@@ -82,7 +82,12 @@
 <main class="bg-light">
 
 <!-- <img src="assets/brand/img/letras_verdes.png" alt="" width="360"> -->
-<h2 class="mb-3 bg-light p-5 text-center featurette-heading" style="margin:3px;"><img src="assets/brand/img/letras_verdes.png" alt="" width="270"></h2>
+<div class="mt-5 mb-3">
+  <h1 class="text-center">
+    <p class="m-0 p-0"><img src="assets/brand/img/logo_store_shoes_sin_fondo.png" alt="" width="270"></p>
+    <p class="m-0 p-0"><img src="assets/brand/img/letras_verdes.png" alt="" width="270"></p>
+  </h1>
+</div>
   <!-- Marketing messaging and featurettes
   ================================================== -->
   <!-- Wrap the rest of the page in another container to center all the content. -->
