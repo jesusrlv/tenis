e2f84e2eec582a78d18cecdb3e312abcfd5cb07b
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-09-19 21:04:50 -0500

    numeros en como pedidos

--- be_/perfil_vendedor/catalogo.php
+++ be_/perfil_vendedor/catalogo.php
@@ -34,7 +34,7 @@ session_start();
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Shoes Store MX · Catálogo</title>
-    <link rel="icon" type="image/png" href="../../assets/brand/img/cel.ico">
+    <link rel="icon" type="image/png" href="../assets/brand/img/cel.ico">
 
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -51,7 +51,7 @@ session_start();
     <script src="query/compra.js"></script>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
       /* normal web */
       
@@ -135,11 +135,11 @@ session_start();
     <?php
     
     // $id_catalogo = $_REQUEST['id'];
-    require('query/query_ini.php');
+    // require('query/query_ini.php');
     ?>
 
     <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
+    <link href="../carousel.css" rel="stylesheet">
   </head>
   <body>
     
