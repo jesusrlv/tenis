54093523907dc92f0ef2477ed57abb9abeb83749
jesusrlv <jesusrlvrojo@gmail.com>
2022-10-03 12:39:23 -0500

    ico en be_

--- be_/perfil_vendedor/catalogo.php
+++ be_/perfil_vendedor/catalogo.php
@@ -37,7 +37,7 @@ session_start();
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Shoes Store MX · Catálogo</title>
-    <link rel="icon" type="image/png" href="assets/brand/img/cel.ico">
+    <link rel="icon" type="image/png" href="../../assets/brand/img/cel.ico">
 
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
