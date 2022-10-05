0d24c8bcbba3b2e3af55014c8ea3574adc493b0a
jesusrlv <jesusrlvrojo@gmail.com>
2022-10-05 14:10:11 -0500

    acomodo guardado

--- be_/perfil_vendedor/query/q_guardar_datos.php
+++ be_/perfil_vendedor/query/q_guardar_datos.php
@@ -47,7 +47,8 @@ $entrega = 1;
 
 $total_precio = $_POST['total_precio']; // para ambos datos
 
-$nombreproducto = $_POST['nombreproducto'];
+// $nombreproducto = $_POST['nombreproducto'];
+$nombreproducto = $_POST['id'];
 $valor = $_POST['valor'];
 
 // venta individual
