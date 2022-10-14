004594c3b490c9ca84236d2753e523ed5c44518a
jesusrlv <jesusrlvrojo@gmail.com>
2022-10-14 16:29:24 -0500

    reportes casi terminado 3

--- be_/reporte_venta_fecha.php
+++ be_/reporte_venta_fecha.php
@@ -111,6 +111,7 @@ else {
   <div class="input-group mb-4 w-50">
     <span class="input-group-text" id="basic-addon1"><i class="bi bi-calendar-week"></i></span>
     <input type="date" class="form-control" placeholder="Buscar por fecha" aria-label="Buscar por fecha" aria-describedby="basic-addon1" id="fecha" name="fecha">
+    <input type="text" value="<?php echo $idReporte?>" name="id" hidden>
     <button type="submit" class="btn btn-primary">Buscar</button>
   </div>
   </form>
