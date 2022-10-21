ea3d9d71000c46893fde82c7434098b8ede19f38
jesusrlv <jesusrlvrojo@gmail.com>
2022-10-21 13:31:43 -0500

    correos fuera de envios

--- query/q_guardar_datos.php
+++ query/q_guardar_datos.php
@@ -68,52 +68,6 @@ $sql_general = "INSERT INTO venta_gral(cantidad,precio,fecha_venta,nombre,direcc
 $resultado_general= $conn->query($sql_general);
 
 if($resultado_general){
-
-    //código email
-    $mail = new PHPMailer(true);
-    
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                                       // Enable verbose debug output
-        $mail->isSMTP();                                            // Set mailer to use SMTP
-        $mail->Host       = 'mail.shoesstoremxa.com';  // Specify main and backup SMTP servers
-        // $mail->Host       = 'mx100.hostgator.mx';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'ventas@shoesstoremxa.com';                     // SMTP username
-        $mail->Password   = 'JsHxj71Ooc7H';                               // SMTP password
-        $mail->SMTPSecure = 'TLS';                                  // Enable TLS encryption, `ssl` also accepted
-        $mail->Port       = 587;                                  // TCP port to connect to
-    
-        //Recipients
-        $mail->setFrom('ventas@shoesstoremxa.com', 'Tienda ShoesStoreMXA');
-        $mail->addAddress($email, $nombre_completo);     // Add a recipient
-        // $mail->addAddress('ellen@example.com');               // Name is optional
-        // $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('tecnologias.injuventud@gmail.com');
-        // $mail->addBCC('bcc@example.com');
-    
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    
-        // Content
-        $mail->isHTML(true);
-        $mail->CharSet = 'UTF-8';                                  // Set email format to HTML
-        $mail->Subject = 'Pedido realizado';
-        $mail->Body    = 'Tu pedido ha sido realizado
-        <p> El pedido a través de shoesstoremxa.com fue completado exitosamente.</p>
-        Tu código de rastreo es el siguiente:'.$codigo.' lo puedes consultar en http://www.shoesstoremxa.com/tenis/envio.php' ;
-        $mail->AltBody = 'Tú pedido se ha realizado exitosamente';
-    
-        $mail->send();
-        // echo 'Message has been sent';
-    
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-
-        
-    //código email
     
     echo "<script type=\"text/javascript\">
     Swal.fire({
