a1c99d9b6ac2ffde901d1e2994bb0b3f53b80811
jesusrlv <jesusrlvrojo@gmail.com>
2022-10-27 18:39:25 -0500

    inicio inner entrega

--- query/query_reporte_entregas.php
+++ query/query_reporte_entregas.php
@@ -8,8 +8,11 @@ $fecha_sistema = strftime("%Y-%m-%d,%H:%M:%S");
     // $sql = "SELECT * FROM venta_gral ORDER BY id DESC LIMIT 30";
     
     //mes y año actual venta Admin
-    $sql = "SELECT * FROM venta_gral WHERE YEAR(fecha_venta) = YEAR(CURRENT_DATE()) 
+    // $sql = "SELECT * FROM venta_gral WHERE YEAR(fecha_venta) = YEAR(CURRENT_DATE()) 
+    // AND MONTH(fecha_venta)  = MONTH(CURRENT_DATE()) AND vendedor = '$idReporte' ORDER BY id DESC";
+    $sql = "SELECT venta_gral.fecha_venta as fecha_venta, venta_gral.cantidad as cantidad FROM venta_gral WHERE YEAR(fecha_venta) = YEAR(CURRENT_DATE()) 
     AND MONTH(fecha_venta)  = MONTH(CURRENT_DATE()) AND vendedor = '$idReporte' ORDER BY id DESC";
+
     $resultado_sql = $conn->query($sql);
     // $row_sql = $resultado_sql->fetch_assoc();
 
